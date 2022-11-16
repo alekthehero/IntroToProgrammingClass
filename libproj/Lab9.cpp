2bbac671db0db535f3abb64db5f39f8b1ac601a1
Alek Bronsky <alekbronsky@gmail.com>
2022-11-16 16:52:32 -0500

    work on lab 9

--- libproj/Lab9.cpp
+++ libproj/Lab9.cpp
@@ -9,31 +9,48 @@ void Lab9() {
 	
 	make_window(800, 600);
 	set_caption("Average Temp");
-
-	ifstream file("data.txt");
+	
+	//get the file
+	string inputFile = open_file_pop_up();
+	
+	ifstream file(inputFile);
 	if (file.fail())
 	{
 		cout << "File not found" << endl;
 		return;
 	}
 	
-	while (true) {
-		//read the data from the file
-		if (file.is_open()) {
-			while (file >> year >> month >> day >> minTemp >> avgTemp >> maxTemp >> maxwind >> snow >> rain) {
-				if (file.fail()) {
-					break;
-				}
-				double xCord = month * 31 + day;
-				draw_point(xCord*2, (get_window_height() - avgTemp)-100);
+	//count for the number of lines in the file
+	int count = 0;
+
+	//set the pen settings
+	set_pen_color(color::black);
+	set_pen_width(2);
+
+	//read the data from the file
+	if (file.is_open()) {
+		//move to first dot and make a point
+		file >> year >> month >> day >> minTemp >> avgTemp >> maxTemp >> maxwind >> snow >> rain;
+		move_to(2.0, (get_window_height() - avgTemp * 5) - 100);
+		draw_point(2.0, (get_window_height() - avgTemp * 5) - 100);
+		
+		//run through the rest of the file
+		while (true) {
+			
+			file >> year >> month >> day >> minTemp >> avgTemp >> maxTemp >> maxwind >> snow >> rain;
+			if (file.fail()) {
+				break;
 			}
-			file.close();
+			count++;
+			double xCord = (2*count)+2;
+			draw_to(xCord, (get_window_height() - avgTemp * 5) - 100);
 		}
-		else {
-			cout << "End Of File" << endl;
-			break;
-		}
+		file.close();
+	}
+	else {
+		cout << "End Of File" << endl;
 	}
 }
+
 
 	
